870cd2aa58ac7eeba19cabc5d3852fb1d6e6c866
Jealls <dewi.mstik@gmail.com>
2020-09-08 19:51:09 +0700

    Dasar Applikasi

---
+++ MarkomApplication.DataModel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkomApplication.DataModel
+{
+    public class Class1
+    {
+    }
+}
